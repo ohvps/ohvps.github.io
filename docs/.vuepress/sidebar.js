ea8e7699535acb25969968a1ad7582e0f62c3dce
Hayal Şensoy <hsensoy@bkm.local>
2022-08-12 14:41:17 +0300

    klasör güncellemesi

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,78 +3,51 @@ module.exports = [
     
     {
         title: 'SÜRÜM 1.0.2 ',
-        path: '/v1.1.1/contents/tanim-ve-kisitlamalar',
+        path: '/v1.0.2/contents/tanim-ve-kisitlamalar',
         children: [
             {
                 title: '1. Tanımlar ve Kısıtlamalar',
-                path: '/v1.1.1/contents/tanim-ve-kisitlamalar'
+                path: '/v1.0.2/contents/tanim-ve-kisitlamalar'
             },
             {
                 title: '2. Kapsam ve Dayanak',
-                path: '/v1.1.1/contents/kapsam-ve-dayanak'
+                path: '/v1.0.2/contents/kapsam-ve-dayanak'
             },
             {
                 title: '3. Temel Prensipler',
-                path: '/v1.1.1/contents/temel-prensipler'
+                path: '/v1.0.2/contents/temel-prensipler'
             },
             {
                 title: '4. Rıza Durumları',
-                path: '/v1.1.1/contents/riza-durumlari'
+                path: '/v1.0.2/contents/riza-durumlari'
             },
             {
                 title: '5. Güçlü Kimlik Doğrulama',
-                path: '/v1.1.1/contents/gkd'                
+                path: '/v1.0.2/contents/gkd'                
             },
             {
                 title: '6. Ödeme Emri Başlatma Hizmeti',
-                path: '/v1.1.1/contents/odeme-emri-baslatma-hizmeti',
-                /*
-                children: [
-                    {
-                        title: '6.1. ADIM 0 - Ödeme Emri Başlatma Isteği',
-                        path: '/v1.0.0/contents/gkd'
-                    },
-                    {
-                        title: '6.2. ADIM 1 - Ödeme Emri Rızasının Hazırlanması',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.3. ADIM 2- Ödeme Emri Rızasının Yetkilendirilmesi',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.4. ADIM 2.1 – Ödeme Emri Rızasının Sorgulanması',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.5. ADIM 3- Ödeme Emrinin Oluşturulması',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.6. ADIM 3.1- Ödeme Emri Sorgusu ',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    }
-                ],
-                */
+                path: '/v1.0.2/contents/odeme-emri-baslatma-hizmeti',
+               
             },
             {
                 title: '7. Hesap Bilgisi Hizmeti',
-                path: '/v1.1.1/contents/hesap-bilgisi-hizmeti',
+                path: '/v1.0.2/contents/hesap-bilgisi-hizmeti',
             
             },
             {
                 title: '8. HHS ve YÖS API',
-                path: '/v1.1.1/contents/hhs-yos-api',
+                path: '/v1.0.2/contents/hhs-yos-api',
             
             },
             {
                 title: '9. Erişim Belirteci API',
-                path: '/v1.1.1/contents/erisim-belirteci',
+                path: '/v1.0.2/contents/erisim-belirteci',
             
             },
             {
                 title: '10. Ekler',
-                path: '/v1.1.1/contents/ekler',
+                path: '/v1.0.2/contents/ekler',
             
             },
         ]
@@ -82,78 +55,51 @@ module.exports = [
 
     {
         title: 'SÜRÜM 1.0.1',
-        path: '/v1.1.0/contents/tanim-ve-kisitlamalar',
+        path: '/v1.0.1/contents/tanim-ve-kisitlamalar',
         children: [
             {
                 title: '1. Tanımlar ve Kısıtlamalar',
-                path: '/v1.1.0/contents/tanim-ve-kisitlamalar'
+                path: '/v1.0.1/contents/tanim-ve-kisitlamalar'
             },
             {
                 title: '2. Kapsam ve Dayanak',
-                path: '/v1.1.0/contents/kapsam-ve-dayanak'
+                path: '/v1.0.1/contents/kapsam-ve-dayanak'
             },
             {
                 title: '3. Temel Prensipler',
-                path: '/v1.1.0/contents/temel-prensipler'
+                path: '/v1.0.1/contents/temel-prensipler'
             },
             {
                 title: '4. Rıza Durumları',
-                path: '/v1.1.0/contents/riza-durumlari'
+                path: '/v1.0.1/contents/riza-durumlari'
             },
             {
                 title: '5. Güçlü Kimlik Doğrulama',
-                path: '/v1.1.0/contents/gkd'                
+                path: '/v1.0.1/contents/gkd'                
             },
             {
                 title: '6. Ödeme Emri Başlatma Hizmeti',
-                path: '/v1.1.0/contents/odeme-emri-baslatma-hizmeti',
-                /*
-                children: [
-                    {
-                        title: '6.1. ADIM 0 - Ödeme Emri Başlatma Isteği',
-                        path: '/v1.0.0/contents/gkd'
-                    },
-                    {
-                        title: '6.2. ADIM 1 - Ödeme Emri Rızasının Hazırlanması',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.3. ADIM 2- Ödeme Emri Rızasının Yetkilendirilmesi',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.4. ADIM 2.1 – Ödeme Emri Rızasının Sorgulanması',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.5. ADIM 3- Ödeme Emrinin Oluşturulması',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    },
-                    {
-                        title: '6.6. ADIM 3.1- Ödeme Emri Sorgusu ',
-                        path: '/v1.0.0/contents/odeme-emri-baslatma-hizmeti'
-                    }
-                ],
-                */
+                path: '/v1.0.1/contents/odeme-emri-baslatma-hizmeti',
+                
             },
             {
                 title: '7. Hesap Bilgisi Hizmeti',
-                path: '/v1.1.0/contents/hesap-bilgisi-hizmeti',
+                path: '/v1.0.1/contents/hesap-bilgisi-hizmeti',
             
             },
             {
                 title: '8. HHS ve YÖS API',
-                path: '/v1.1.0/contents/hhs-yos-api',
+                path: '/v1.0.1/contents/hhs-yos-api',
             
             },
             {
                 title: '9. Erişim Belirteci API',
-                path: '/v1.1.0/contents/erisim-belirteci',
+                path: '/v1.0.1/contents/erisim-belirteci',
             
             },
             {
                 title: '10. Ekler',
-                path: '/v1.1.0/contents/ekler',
+                path: '/v1.0.1/contents/ekler',
             
             },
         ]
