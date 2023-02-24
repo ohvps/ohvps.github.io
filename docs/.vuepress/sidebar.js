a90988a8df3fad50245878d27bbf13292f1d864c
az3 <azureel@gmail.com>
2023-02-24 16:41:26 +0300

    Sol menü bağlantıları v1.0.3 için güncellendi.

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,6 +1,57 @@
 module.exports = [
 
-    
+    {
+        title: 'SÜRÜM 1.0.3 ',
+        path: '/v1.0.3/contents/tanim-ve-kisitlamalar',
+        children: [
+            {
+                title: '1. Tanımlar ve Kısıtlamalar',
+                path: '/v1.0.3/contents/tanim-ve-kisitlamalar'
+            },
+            {
+                title: '2. Kapsam ve Dayanak',
+                path: '/v1.0.3/contents/kapsam-ve-dayanak'
+            },
+            {
+                title: '3. Temel Prensipler',
+                path: '/v1.0.3/contents/temel-prensipler'
+            },
+            {
+                title: '4. Rıza Durumları',
+                path: '/v1.0.3/contents/riza-durumlari'
+            },
+            {
+                title: '5. Güçlü Kimlik Doğrulama',
+                path: '/v1.0.3/contents/gkd'                
+            },
+            {
+                title: '6. Ödeme Emri Başlatma Hizmeti',
+                path: '/v1.0.3/contents/odeme-emri-baslatma-hizmeti',
+               
+            },
+            {
+                title: '7. Hesap Bilgisi Hizmeti',
+                path: '/v1.0.3/contents/hesap-bilgisi-hizmeti',
+            
+            },
+            {
+                title: '8. HHS ve YÖS API',
+                path: '/v1.0.3/contents/hhs-yos-api',
+            
+            },
+            {
+                title: '9. Erişim Belirteci API',
+                path: '/v1.0.3/contents/erisim-belirteci',
+            
+            },
+            {
+                title: '10. Ekler',
+                path: '/v1.0.3/contents/ekler',
+            
+            },
+        ]
+    },
+
     {
         title: 'SÜRÜM 1.0.2 ',
         path: '/v1.0.2/contents/tanim-ve-kisitlamalar',
