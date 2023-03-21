6dc03969e19de9cf431c9348936ea130f63adf2f
Hayal Şensoy <hsensoy@bkm.local>
2023-03-21 17:16:45 +0300

    x-total-count koşullu hale getirilmiştir

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,56 +1,6 @@
 module.exports = [
 
-    {
-        title: 'SÜRÜM 1.1.0 - TASLAK ',
-        path: '/v1.1.0/contents/tanim-ve-kisitlamalar',
-        children: [
-            {
-                title: '1. Tanımlar ve Kısıtlamalar',
-                path: '/v1.1.0/contents/tanim-ve-kisitlamalar'
-            },
-            {
-                title: '2. Kapsam ve Dayanak',
-                path: '/v1.1.0/contents/kapsam-ve-dayanak'
-            },
-            {
-                title: '3. Temel Prensipler',
-                path: '/v1.1.0/contents/temel-prensipler'
-            },
-            {
-                title: '4. Rıza Durumları',
-                path: '/v1.1.0/contents/riza-durumlari'
-            },
-            {
-                title: '5. Güçlü Kimlik Doğrulama',
-                path: '/v1.1.0/contents/gkd'                
-            },
-            {
-                title: '6. Ödeme Emri Başlatma Hizmeti',
-                path: '/v1.1.0/contents/odeme-emri-baslatma-hizmeti',
-               
-            },
-            {
-                title: '7. Hesap Bilgisi Hizmeti',
-                path: '/v1.1.0/contents/hesap-bilgisi-hizmeti',
-            
-            },
-            {
-                title: '8. HHS ve YÖS API',
-                path: '/v1.1.0/contents/hhs-yos-api',
-            
-            },
-            {
-                title: '9. Erişim Belirteci API',
-                path: '/v1.1.0/contents/erisim-belirteci',
-            
-            },
-            {
-                title: '10. Ekler',
-                path: '/v1.1.0/contents/ekler',
-            
-            },
-        ]
-    },
+ 
     {
         title: 'SÜRÜM 1.0.2 ',
         path: '/v1.0.2/contents/tanim-ve-kisitlamalar',
@@ -154,6 +104,57 @@ module.exports = [
             },
         ]
     } ,
+    {
+        title: 'SÜRÜM 1.1.0 - TASLAK ',
+        path: '/v1.1.0/contents/tanim-ve-kisitlamalar',
+        children: [
+            {
+                title: '1. Tanımlar ve Kısıtlamalar',
+                path: '/v1.1.0/contents/tanim-ve-kisitlamalar'
+            },
+            {
+                title: '2. Kapsam ve Dayanak',
+                path: '/v1.1.0/contents/kapsam-ve-dayanak'
+            },
+            {
+                title: '3. Temel Prensipler',
+                path: '/v1.1.0/contents/temel-prensipler'
+            },
+            {
+                title: '4. Rıza Durumları',
+                path: '/v1.1.0/contents/riza-durumlari'
+            },
+            {
+                title: '5. Güçlü Kimlik Doğrulama',
+                path: '/v1.1.0/contents/gkd'                
+            },
+            {
+                title: '6. Ödeme Emri Başlatma Hizmeti',
+                path: '/v1.1.0/contents/odeme-emri-baslatma-hizmeti',
+               
+            },
+            {
+                title: '7. Hesap Bilgisi Hizmeti',
+                path: '/v1.1.0/contents/hesap-bilgisi-hizmeti',
+            
+            },
+            {
+                title: '8. HHS ve YÖS API',
+                path: '/v1.1.0/contents/hhs-yos-api',
+            
+            },
+            {
+                title: '9. Erişim Belirteci API',
+                path: '/v1.1.0/contents/erisim-belirteci',
+            
+            },
+            {
+                title: '10. Ekler',
+                path: '/v1.1.0/contents/ekler',
+            
+            },
+        ]
+    },
     {
         title: 'Sürüm Yönetimi',
         path: '/surum-yonetimi'
